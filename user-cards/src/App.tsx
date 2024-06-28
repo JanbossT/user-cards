9ee63d148501b9cb183b645cac27572b1e93f2c0
JanbossT <janbotoktobolotov1@gmail.com>
2024-06-28 15:11:08 +0600

    App.tsx

--- user-cards/src/App.tsx
+++ user-cards/src/App.tsx
@@ -1,32 +1,43 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from './store';
+import { clearToken } from './store/authSlice';
 import Home from './pages/Home';
 import UserPage from './pages/UserPage';
 import Login from './pages/Login';
 
 const App: React.FC = () => {
   const token = useSelector((state: RootState) => state.auth.token);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(clearToken());
+  };
 
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {token ? (
-          <>
-            <Route path="/" element={<Home />} />
-            <Route path="/users/:id" element={<UserPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        ) : (
-          <Route path="*" element={<Navigate to="/login" />} />
+      <div>
+        {token && (
+          <nav>
+            <button onClick={handleLogout}>Logout</button>
+          </nav>
         )}
-      </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {token ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/users/:id" element={<UserPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          )}
+        </Routes>
+      </div>
     </Router>
   );
 };
 
 export default App;
-
